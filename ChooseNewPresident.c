e2501253161d9a53e55eb1ca57ad7a20a49fe33e
Aya Ali <101468593+AyaA1i@users.noreply.github.com>
2024-05-07 00:59:11 +0300

    version 2

--- ChooseNewPresident.c
+++ ChooseNewPresident.c
@@ -4,26 +4,126 @@
 #include <stdbool.h>
 #include <mpi.h>
 
-int getNumberCount(char fileName[], int x)
+// get num of candidates and voters from the file
+void getCounts(int *candidatesCount, int *votersCount, FILE *fptr)
 {
-    FILE *file = fopen(fileName, "r");
-    if (file == NULL)
+    char input[100];
+    int lines = 1;
+    while (fgets(input, 100, fptr) && lines <= 2)
     {
-        return -1;
-    }
-    int count = 0;
-    int number;
-    while (fscanf(file, "%d", &number) == 1)
-    {
-        if (number == x)
+        if (lines == 1)
         {
-            count++;
+            *candidatesCount = atoi(input);
         }
+        else
+        {
+            *votersCount = atoi(input);
+        }
+        lines++;
     }
-    fclose(file);
-    return count;
+}
+// get voters from file to every process
+void getMyVoters(int (*votes)[100], int candidatesCount, int startPos, int myCount, FILE *fptr)
+{
+    char input[100];
+    int lines = 1;
+    int i = 0;
+    while (fgets(input, 100, fptr) && lines < startPos + myCount)
+    {
+
+        if (lines >= startPos)
+        {
+            int j = 0, k = 0;
+            for (; j < candidatesCount; j++)
+            {
+                int l = 0;
+                char tmp[100];
+                while (k < strlen(input) && input[k] != ' ')
+                {
+                    tmp[l] = input[k];
+                    l++;
+                    k++;
+                }
+                if (input[k] == ' ')
+                    k++;
+
+                votes[i][j] = atoi(tmp);
+            }
+            i++;
+        }
+        lines++;
+    }
 }
 
+void getRoundsVotes(int candidatesCount, int votes[][100], int *freq, int round, int index1, int index2)
+{
+    int j = 0;
+    for (; j < candidatesCount; j++)
+    {
+        if (round == 1)
+        {
+            freq[votes[j][0]]++;
+        }
+        else
+        {
+            int k = 0;
+            for (; k < candidatesCount; k++)
+            {
+                if (votes[j][k] == index1 || votes[j][k] == index2)
+                {
+                    freq[votes[j][k]]++;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void getMax2(int candidatesCount, int allFreq[100], int *index1, int *index2)
+{
+    int mx1 = 0, mx2 = 0, j = 1;
+    for (; j <= candidatesCount; j++)
+    {
+        if (allFreq[j] >= mx1)
+        {
+            mx1 = allFreq[j];
+            *index1 = j;
+        }
+    }
+    j = 1;
+    for (; j <= candidatesCount; j++)
+    {
+        if (allFreq[j] >= mx2 && j != *index1)
+        {
+            mx2 = allFreq[j];
+            *index2 = j;
+        }
+    }
+}
+void print(int candidatesCount,int votersCount,
+ int allFreq[100], int index1, int index2,int round, int *winnerId, bool *winner, int *winnerPerc)
+{
+    int j = 1;
+    for (; j <= candidatesCount; j++)
+    {
+        if (round == 2)
+        {
+            if (j != index1 && j != index2)
+                continue;
+        }
+
+        double f = allFreq[j];
+
+        double percent = (f / votersCount) * 100;
+        if (percent > 50)
+        {
+            *winnerId = j;
+            *winner = true;
+            *winnerPerc = percent;
+        }
+        printf("Candidate %d : %f \n", j, percent);
+    }
+}
 int main(int argc, char *argv[])
 {
     int my_rank;
@@ -33,16 +133,15 @@ int main(int argc, char *argv[])
     int round = 1;
     int index1 = 0, index2 = 0;
     bool winner = false;
+    char filename[100];
+    int candidatesCount;
+    int votersCount = 0;
+    int myCount;
 
     MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
-
-    char filename[100];
-    int candidatesCount;
-    int votersCount = 0;
-    int myCount;
 
     if (my_rank == 0)
     {
@@ -51,20 +150,7 @@ int main(int argc, char *argv[])
         scanf("%s", filename);
         FILE *fptr;
         fptr = fopen(filename, "r");
-        char input[100];
-        int lines = 1;
-        while (fgets(input, 100, fptr) && lines <= 2)
-        {
-            if (lines == 1)
-            {
-                candidatesCount = atoi(input);
-            }
-            else
-            {
-                votersCount = atoi(input);
-            }
-            lines++;
-        }
+        getCounts(&candidatesCount, &votersCount, fptr);
         myCount = votersCount / p;
         int i;
         for (i = 1; i < p; i++)
@@ -91,79 +177,16 @@ int main(int argc, char *argv[])
             FILE *fptr;
             fptr = fopen(filename, "r");
             int votes[1000][100];
-            char input[100];
-            int lines = 1;
             int startPos = (my_rank * myCount) + 2;
-            int i = 0;
-            while (fgets(input, 100, fptr) && lines < startPos + myCount)
-            {
-
-                if (lines >= startPos)
-                {
-                    int j = 0, k = 0;
-                    for (; j < candidatesCount; j++)
-                    {
-                        int l = 0;
-                        char tmp[100];
-                        while (k < strlen(input) && input[k] != ' ')
-                        {
-                            tmp[l] = input[k];
-                            l++;
-                            k++;
-                        }
-                        if (input[k] == ' ')
-                            k++;
-
-                        votes[i][j] = atoi(tmp);
-                        // if(round == 2){
-                        //     printf("%d \n" ,votes[i][j] );
-                        // }
-                    }
-                    i++;
-                }
-                lines++;
-            }
-            int q = 0;
-            for (; q < 100; q++)
-            {
-                freq[q] = 0;
-            }
-
-            int j = 0;
-            for (; j <= i; j++)
-            {
-                if (round == 1)
-                {
-                    freq[votes[j][0]]++;
-                }
-                else
-                {
-                    printf("CC %d\n", candidatesCount);
-                    int k = 0;
-                    for (; k < candidatesCount; k++)
-                    {
-
-                        // printf("index1 :  %d - index2 : %d\n", index1, index2);
-                        if (votes[j][k] == index1 || votes[j][k] == index2)
-                        {
-                            freq[votes[j][k]]++;
-                            // printf("freq %d : %d round %d\n", votes[j][k], freq[votes[j][k]], round);
-                            break;
-                        }
-                    }
-                }
-            }
+            getMyVoters(votes, candidatesCount, startPos, myCount, fptr);
+            memset(freq, 0, sizeof(freq));
+            getRoundsVotes(candidatesCount, votes, freq, round, index1, index2);
             MPI_Send(&freq, 100, MPI_INT, 0, tag, MPI_COMM_WORLD);
         }
         else
         {
             int allFreq[100];
-            int q = 0;
-            for (; q < 100; q++)
-            {
-                allFreq[q] = 0;
-            }
-
+            memset(allFreq, 0, sizeof(allFreq));
             int i = 1;
             for (; i < p; i++)
             {
@@ -179,80 +202,12 @@ int main(int argc, char *argv[])
             FILE *fptr;
             fptr = fopen(filename, "r");
             int votes[1000][100];
-            char input[100];
-            int lines = 1;
-            while (fgets(input, 100, fptr))
-            {
-
-                if (lines >= startPos)
-                {
-                    int j = 0, k = 0;
-                    for (; j < candidatesCount; j++)
-                    {
-                        int l = 0;
-                        char tmp[100];
-                        while (k < strlen(input) && input[k] != ' ')
-                        {
-                            tmp[l] = input[k];
-                            l++;
-                            k++;
-                        }
-                        if (input[k] == ' ')
-                            k++;
-
-                        votes[i][j] = atoi(tmp);
-                    }
-                    i++;
-                }
-                lines++;
-            }
-            fclose(fptr);
-
-            int j = 0;
-            for (; j <= i; j++)
-            {
-                if (round == 1)
-                {
-                    allFreq[votes[j][0]]++;
-                }
-                else
-                {
-                    int k = 0;
-                    for (; k < candidatesCount; k++)
-                    {
-                        if (votes[j][k] == index1 || votes[j][k] == index2)
-                        {
-                            allFreq[votes[j][k]]++;
-                            break;
-                        }
-                    }
-                }
-            }
-            j = 1;
+            getMyVoters(votes, candidatesCount, startPos, 1000, fptr);
+            getRoundsVotes(candidatesCount, votes, allFreq, round, index1, index2);
+            int j = 1;
             printf("Round %d :\n", round);
             int winnerId, winnerPerc;
-            for (; j <= candidatesCount; j++)
-            {
-                if (round == 2)
-                {
-                    printf("freq %d : %d", j, allFreq[j]);
-                    if (j != index1 && j != index2)
-                        continue;
-                    // votersCount = 2;
-                }
-
-                double f = allFreq[j];
-
-                double percent = (f / votersCount) * 100;
-                if (percent > 50)
-                {
-                    winnerId = j;
-                    winner = true;
-                    winnerPerc = percent;
-                }
-                // printf("Candidate %d : %d\n" , j , allFreq[j]);
-                printf("Candidate %d : %f \n", j, percent);
-            }
+            print(candidatesCount,votersCount,allFreq,index1, index2,round, &winnerId, &winner, &winnerPerc);
             if (winner)
             {
                 printf("Candidate %d with percentage %d won in round %d\n", winnerId, winnerPerc, round);
@@ -260,34 +215,14 @@ int main(int argc, char *argv[])
             }
             else
             {
-                int mx1 = 0, mx2 = 0, j = 1;
-                for (; j <= candidatesCount; j++)
-                {
-                    if (allFreq[j] >= mx1)
-                    {
-                        mx1 = allFreq[j];
-                        index1 = j;
-                    }
-                }
-                j = 1;
-                for (; j <= candidatesCount; j++)
-                {
-                    if (allFreq[j] >= mx2 && j != index1)
-                    {
-                        mx2 = allFreq[j];
-                        index2 = j;
-                    }
-                }
+                getMax2(candidatesCount, allFreq, &index1, &index2);
                 int i;
-                printf("my count %d\n", myCount);
                 for (i = 1; i < p; i++)
                 {
                     MPI_Send(&myCount, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
                     MPI_Send(&index1, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
                     MPI_Send(&index2, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
                 }
-                // printf("%d indx1 - %d indx2 \n",index1,index2);
-                //  MPI_Send(&round, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
             }
         }
     }
